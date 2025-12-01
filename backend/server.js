3722a502119499f8383cc31642c7cabaa9792684
Temmar-soukaina <soukainatemmar63@gmail.com>
2025-12-01 14:42:18 +0100

    configur path for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const connectDB = require('./dbconnect');
 const cors = require('cors');
 const app = express();
 app.use(cors());
+const path = require('path');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,12 @@ const authRoutes = require('./routes/auth');
 const complaintRoutes = require('./routes/complaint');
 app.use('/api', complaintRoutes);
 app.use('/api', authRoutes);
+
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
